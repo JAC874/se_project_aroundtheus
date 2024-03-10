fa3c26926805b9e6cef52ea28f01fd343e43ad64
Jordon Cunningham <153477169+JAC874@users.noreply.github.com>
2024-03-10 19:22:50 -0400

    stage 4 initial upload

--- scripts/index.js
+++ scripts/index.js
@@ -36,7 +36,11 @@ const profileTitleInput = document.querySelector("#profile-title-input");
 const profileDescriptionInput = document.querySelector(
   "#profile-description-input"
 );
+
 const profileEditForm = profileEditModal.querySelector(".modal__profile-form");
+const cardListEl = document.querySelector(".cards__list");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
 
 //FUNCTIONS
 
@@ -53,7 +57,7 @@ function handleProfileEditSubmit(e) {
   closePopUp();
 }
 
-//EVENT LISTENERS
+// EVENT LISTENERS
 
 profileEditButton.addEventListener("click", () => {
   profileTitleInput.value = profileTitle.textContent;
@@ -64,3 +68,14 @@ profileEditButton.addEventListener("click", () => {
 profileModalCloseButton.addEventListener("click", closePopUp);
 
 profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+initialCards.forEach((cardData) => {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardImageEl = cardElement.querySelector(".card__image");
+  const cardTitleEl = cardElement.querySelector(".card__title");
+  cardImageEl.src = cardData.link;
+  cardImageEl.alt = cardData.name;
+  cardTitleEl.textContent = cardData.name;
+  cardListEl.append(cardElement);
+  return cardElement;
+});
