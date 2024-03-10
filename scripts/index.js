9c7fbca3639300a5f56e59a8261dc51608b57b15
Jordon Cunningham <153477169+JAC874@users.noreply.github.com>
2024-03-10 01:20:53 -0500

    add js for open and close profile buttons

--- scripts/index.js
+++ scripts/index.js
@@ -24,3 +24,14 @@ const initialCards = [
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
   },
 ];
+
+const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileEditButton = document.querySelector("#profile-edit-button");
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal_opened");
+});
+
+const profileModalCloseButton = document.querySelector("#profile-close-button");
+profileModalCloseButton.addEventListener("click", () => {
+  profileEditModal.classList.remove("modal_opened");
+});
